3164ecbd55156d634132dce2e29e7176568157e4
UserNanonic <serge.allard@gmail.com>
2022-11-07 22:01:03 -0500

    Section 2 lecture 13 done

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,60 @@
+//Simple variable
+
+//Primitive data type
+//Number 
+let age = 23;
+
+//String
+let employeeName = "Serge";
+
+//Boolean
+let hasJob = true;
+
+//Undefined
+let undefVariable;
+
+//Null
+let nullVar = null;
+
+//Symbols - not used for now
+
+//BigInt
+
+console.log(typeof hasJob);
+console.log(typeof age);
+console.log(undefVariable);
+
+//Dynamic typing
+age = "serge";
+console.log(typeof age);
+
+console.log(nullVar)
+
+//Ways of declaring variable
+let varAge = 30;
+age = 31; //Mutate the variable
+
+const BIRTHYEAR = 1968; //Immutable variable
+//BIRTHYEAR = 2001; //Assignment to constant variable. Error
+
+var varJOb = "Programmer"; //Old way of declaring variable
+varX1 = "Undeclared variable";
+console.log(varX1);
+
+
+/********************************************************/
+//Assignment
+//Simple variable
+console.log("----------Assignment-");
+const country = "Canada";
+const continent = "North America";
+let population = 35000000;
+const isIsland = false;
+const language = "French";
+
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(language);
+console.log(isIsland);
+
